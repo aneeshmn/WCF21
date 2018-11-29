658611ccb0b63a7c349f42cf6bf34ba681440adc
aneeshmn <45420040+aneeshmn@users.noreply.github.com>
2018-11-28 23:40:44 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using WCFAssignment_1DOTNET;
+
+namespace SelfHost
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //Create a URI to serve as the base address
+            Uri httpUrl = new Uri("http://localhost:8733/Design_Time_Addresses/WCFAssignment_1DOTNET/Service1/SayHello");
+            //Create ServiceHost
+            ServiceHost host = new ServiceHost(typeof(WCFAssignment_1DOTNET.Service1), httpUrl);
+            //Add a service endpoint
+            host.AddServiceEndpoint(typeof(WCFAssignment_1DOTNET.IService1)
+            , new WSHttpBinding(), "");
+            //Enable metadata exchange
+            ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+            smb.HttpGetEnabled = true;
+            host.Description.Behaviors.Add(smb);
+            //Start the Service
+            host.Open();
+
+            Console.WriteLine("Service is host at " + DateTime.Now.ToString());
+            Console.WriteLine("Host is running... Press <Enter> key to stop");
+            Console.ReadLine();
+        }
+    }
+}
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CalculatorWCFService")]
+[assembly: AssemblyTitle("SelfHost")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CalculatorWCFService")]
+[assembly: AssemblyProduct("SelfHost")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("25a80e1f-a9b8-4636-b414-c5170a7b67c8")]
+[assembly: Guid("12549b6b-5726-4109-aae8-eed55e19780b")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
